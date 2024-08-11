a31973ffde4d9caaa11d60315b6a3ae797d86ef9
Administrator <Administrator@DESKTOP-2FJNJFC>
2024-08-11 17:54:15 +0800

    定时任务变更为每15分钟执行一次

--- trading-app/src/main/java/com/bugbycode/trading_app/task/LastDayPriceListenTask.java
+++ trading-app/src/main/java/com/bugbycode/trading_app/task/LastDayPriceListenTask.java
@@ -35,11 +35,11 @@ public class LastDayPriceListenTask {
 	private KlinesService klinesService;
 	
 	/**
-	 * 查询k线信息 每五分钟执行一次
+	 * 查询k线信息 每十五分钟执行一次
 	 * 
 	 * @throws Exception
 	 */
-	@Scheduled(cron = "4 0/5 * * * ?")
+	@Scheduled(cron = "4 0/15 * * * ?")
 	public void continuousKlines() throws Exception {
 		logger.info("LconicHighAndLowPricesListenTask start.");
 		Date now = new Date();
@@ -77,17 +77,17 @@ public class LastDayPriceListenTask {
 				List<Klines> klinesList_1_x_day_15m = klinesService.continuousKlines(pair, restBaseUrl, lastDayStartTimeDate.getTime(), 
 						lastDayEndTimeDate.getTime(), AppConfig.INERVAL_15M);
 				
-				//5分钟级别K线起止时间
-				Date endTime_5m = DateFormatUtil.parse(DateFormatUtil.format_yyyy_mm_dd_HH_mm_00(now));
-				Date startTime_5m = DateFormatUtil.getStartTimeBySetMinute(endTime_5m, -5);//5分钟以前开盘时间
-				endTime_5m = DateFormatUtil.getStartTimeBySetSecond(endTime_5m, -1);//收盘时间
+				//15分钟级别K线起止时间
+				Date endTime_15m = DateFormatUtil.parse(DateFormatUtil.format_yyyy_mm_dd_HH_mm_00(now));
+				Date startTime_15m = DateFormatUtil.getStartTimeBySetMinute(endTime_15m, -15);//15分钟以前开盘时间
+				endTime_15m = DateFormatUtil.getStartTimeBySetSecond(endTime_15m, -1);//收盘时间
 				
-				//前1根5分钟级别k线信息
-				List<Klines> klinesList_1_x_5m = klinesService.continuousKlines(pair, restBaseUrl, startTime_5m.getTime(),
-						endTime_5m.getTime(), AppConfig.INERVAL_5M);
+				//前1根15分钟级别k线信息
+				List<Klines> klinesList_1_x_15m = klinesService.continuousKlines(pair, restBaseUrl, startTime_15m.getTime(),
+						endTime_15m.getTime(), AppConfig.INERVAL_15M);
 				
-				if(klinesList_1_x_5m.isEmpty()) {
-					logger.info("无法获取" + pair + "交易对最近5分钟K线信息");
+				if(klinesList_1_x_15m.isEmpty()) {
+					logger.info("无法获取" + pair + "交易对最近15分钟K线信息");
 					continue;
 				}
 				
@@ -96,8 +96,8 @@ public class LastDayPriceListenTask {
 				}
 				
 				//Klines kline_1d = klinesList_1_x_day.get(0);
-				Klines kline_5m = klinesList_1_x_5m.get(0);
-				int decimalNum = kline_5m.getDecimalNum();
+				Klines kline_15m = klinesList_1_x_15m.get(0);
+				int decimalNum = kline_15m.getDecimalNum();
 				/*
 				//昨日开盘、收盘、最低、最高价格
 				double closePrice_1d = kline_1d.getClosePrice();
@@ -105,12 +105,12 @@ public class LastDayPriceListenTask {
 				double lowPrice_1d = kline_1d.getLowPrice();
 				double hightPrice_1d = kline_1d.getHighPrice();
 				*/
-				//5分钟开盘、收盘、最低、最高价格
-				double closePrice_5m = kline_5m.getClosePrice();
-				//double openPrice_5m = kline_5m.getOpenPrice();
-				double lowPrice_5m = kline_5m.getLowPrice();
-				double hightPrice_5m = kline_5m.getHighPrice();
-				double currentPrice = closePrice_5m;
+				//15分钟开盘、收盘、最低、最高价格
+				double closePrice_15m = kline_15m.getClosePrice();
+				//double openPrice_15m = kline_15m.getOpenPrice();
+				double lowPrice_15m = kline_15m.getLowPrice();
+				double hightPrice_5m = kline_15m.getHighPrice();
+				double currentPrice = closePrice_15m;
 				
 				Klines highKline_1d_15m = PriceUtil.getMaxPriceKLine(klinesList_1_x_day_15m);//昨日最高价k线信息
 				Klines lowKline_1d_15m = PriceUtil.getMinPriceKLine(klinesList_1_x_day_15m);//昨日最低价k线信息
@@ -156,44 +156,44 @@ public class LastDayPriceListenTask {
 					}
 					
 					//空头行情中可做多的情况 最低价
-					else if(currentPrice >= fib0 && lowPrice_5m <= fib0) {//最低价做多
+					else if(currentPrice >= fib0 && lowPrice_15m <= fib0) {//最低价做多
 						subject = pair + "永续合约【极好的】做多机会 " + DateFormatUtil.format(new Date());
 						//理想止盈价 fib618
 						//理想止损价 lowPrice_5m
-						text = StringUtil.formatLongMessage(pair, currentPrice, lowPrice_5m, fib618,decimalNum);
-					} else if(currentPrice >= fib236 && lowPrice_5m <= fib236) {//0.236做多
+						text = StringUtil.formatLongMessage(pair, currentPrice, lowPrice_15m, fib618,decimalNum);
+					} else if(currentPrice >= fib236 && lowPrice_15m <= fib236) {//0.236做多
 						subject = pair + "永续合约【不错的】做多机会 " + DateFormatUtil.format(new Date());
 						//理想止盈价 fib618
 						//理想止损价 lowPrice_5m
-						text = StringUtil.formatLongMessage(pair, currentPrice, lowPrice_5m, fib618,decimalNum);
+						text = StringUtil.formatLongMessage(pair, currentPrice, lowPrice_15m, fib618,decimalNum);
 					}
 					
 				} else if(fib0 > fib1) {//多头行情
 					subject = pair + "永续合约做多机会 " + DateFormatUtil.format(new Date());
 					
 					//未能跌破最低价的价格行为
-					if(currentPrice >= fib1 && lowPrice_5m <= fib1) {//最低价做多
+					if(currentPrice >= fib1 && lowPrice_15m <= fib1) {//最低价做多
 						//理想止盈价 fib382
 						//理想止损价 lowPrice_5m
-						text = StringUtil.formatLongMessage(pair, currentPrice, lowPrice_5m, fib382,decimalNum);
+						text = StringUtil.formatLongMessage(pair, currentPrice, lowPrice_15m, fib382,decimalNum);
 						
 						subject = pair + "永续合约【极好的】做多机会 " + DateFormatUtil.format(new Date());
 						
-					} else if(currentPrice >= fib786 && lowPrice_5m <= fib786) {//0.786做多
+					} else if(currentPrice >= fib786 && lowPrice_15m <= fib786) {//0.786做多
 						//理想止盈价 fib382
 						//理想止损价 lowPrice_5m
-						text = StringUtil.formatLongMessage(pair, currentPrice, lowPrice_5m, fib382,decimalNum);
+						text = StringUtil.formatLongMessage(pair, currentPrice, lowPrice_15m, fib382,decimalNum);
 						
 						subject = pair + "永续合约【不错的】做多机会 " + DateFormatUtil.format(new Date());
 						
-					} else if(currentPrice >= fib66 && lowPrice_5m <= fib66) {//0.66做多
+					} else if(currentPrice >= fib66 && lowPrice_15m <= fib66) {//0.66做多
 						//理想止盈价 fib382
 						//理想止损价 lowPrice_5m
-						text = StringUtil.formatLongMessage(pair, currentPrice, lowPrice_5m, fib382,decimalNum);
-					} else if(currentPrice >= fib618 && lowPrice_5m <= fib618) {//0.618做多
+						text = StringUtil.formatLongMessage(pair, currentPrice, lowPrice_15m, fib382,decimalNum);
+					} else if(currentPrice >= fib618 && lowPrice_15m <= fib618) {//0.618做多
 						//理想止盈价 fib382
 						//理想止损价 lowPrice_5m
-						text = StringUtil.formatLongMessage(pair, currentPrice, lowPrice_5m, fib382,decimalNum);
+						text = StringUtil.formatLongMessage(pair, currentPrice, lowPrice_15m, fib382,decimalNum);
 					}
 					
 					//多头行情可做空的情况 最高价
@@ -278,7 +278,7 @@ public class LastDayPriceListenTask {
 						}
 						
 						//价格假突破高点行为 收盘价小于等于标志性高点 最高价大于等于标志性高点
-						if(closePrice_5m <= maxLconicHighPrice) {
+						if(closePrice_15m <= maxLconicHighPrice) {
 							//理想止盈价 fib382
 							//理想止损价 hightPrice_5m
 							text = StringUtil.formatShortMessage(pair, currentPrice, fib382, hightPrice_5m,decimalNum);
@@ -315,16 +315,16 @@ public class LastDayPriceListenTask {
 							
 							//logger.info("匹配K线：" + klines.toString());
 							
-							if(minLconicLowPrice < lowPrice_5m) {//遇到更低的低点时直接退出
+							if(minLconicLowPrice < lowPrice_15m) {//遇到更低的低点时直接退出
 								//logger.info("当前k线最低价小于当前五分钟k线最低价，因此无需继续匹配");
 								break;
 							}
 							
 							//假跌破 收盘价大于等于标志性低点 最低价小于等于标志性低点
-							if(closePrice_5m >= minLconicLowPrice) {
+							if(closePrice_15m >= minLconicLowPrice) {
 								//理想止盈价 fib382
 								//理想止损价 lowPrice_5m
-								text = StringUtil.formatLongMessage(pair, currentPrice, lowPrice_5m, fib382,decimalNum);
+								text = StringUtil.formatLongMessage(pair, currentPrice, lowPrice_15m, fib382,decimalNum);
 								
 								subject = pair + "永续合约【难得的】做多机会 " + DateFormatUtil.format(new Date());
 								
