f3d5b86575bf528cd6dc5e4be608bce0b0b33e4a
bugbycode <bugbycode@gmail.com>
2024-09-27 01:00:40 +0800

    停用涨跌分析

--- trading-app/src/main/java/com/bugbycode/trading_app/task/sync/work/AnalysisKlinesTask.java
+++ trading-app/src/main/java/com/bugbycode/trading_app/task/sync/work/AnalysisKlinesTask.java
@@ -60,19 +60,17 @@ public class AnalysisKlinesTask implements Runnable{
             //klinesService.futuresFibMonitor(klines_list_1d, klines_list_15m);
             
             //涨跌分析
-            klinesService.futuresRiseAndFall(klines_list_15m);
+            //klinesService.futuresRiseAndFall(klines_list_15m);
 
             //EMA指标分析
             //klinesService.futuresEMAMonitor(klines_list_15m);
 
             //EMA指标涨跌判断
-            klinesService.futuresEmaRiseAndFall(klines_list_15m);
+            //klinesService.futuresEmaRiseAndFall(klines_list_15m);
 
             //标志性高低点分析
             
-            if("BTCUSDT".equals(pair) || "ETHUSDT".equals(pair)) {
-                klinesService.futuresHighOrLowMonitor(klines_list_1d, klines_list_15m);
-            }
+            klinesService.futuresHighOrLowMonitor(klines_list_1d, klines_list_15m);
 
         } catch (Exception e) {
             logger.error("分析" + pair + "交易对K线信息时出现异常", e);
