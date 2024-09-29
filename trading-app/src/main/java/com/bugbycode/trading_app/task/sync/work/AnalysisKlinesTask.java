8b5dadcf748db726a18f8962592549dc994342cd
bugbycode <bugbycode@gmail.com>
2024-09-29 23:03:37 +0800

    启用标志性高低点指标

--- trading-app/src/main/java/com/bugbycode/trading_app/task/sync/work/AnalysisKlinesTask.java
+++ trading-app/src/main/java/com/bugbycode/trading_app/task/sync/work/AnalysisKlinesTask.java
@@ -57,31 +57,21 @@ public class AnalysisKlinesTask implements Runnable{
             if(!klinesRepository.checkData(klines_list_15m)) {
             	klines_list_15m = klinesRepository.findLastKlinesByPair(pair, Inerval.INERVAL_15M.getDescption(),5000);
             }
-
+            
             //斐波那契回撤分析
             //klinesService.futuresFibMonitor(klines_list_1d, klines_list_15m);
-            /*
-            Klines last15m = PriceUtil.getLastKlines(klines_list_15m);
             
-            int minute = DateFormatUtil.getMinute(last15m.getEndTime());
-            
-            if(minute == 59) {
-                List<Klines> klines_list_1h = PriceUtil.to1HFor15MKlines(klines_list_15m);
-                //涨跌分析
-                klinesService.futuresRiseAndFall(klines_list_1h);
-            } else {
-                //涨跌分析
-                klinesService.futuresRiseAndFall(klines_list_15m);
-            }*/
+            //涨跌分析
+            //klinesService.futuresRiseAndFall(klines_list_15m);
 
             //EMA指标分析
-            klinesService.futuresEMAMonitor(klines_list_15m);
+            //klinesService.futuresEMAMonitor(klines_list_15m);
 
             //EMA指标涨跌判断
-            klinesService.futuresEmaRiseAndFall(klines_list_15m);
+            //klinesService.futuresEmaRiseAndFall(klines_list_15m);
 
             //标志性高低点分析
-            //klinesService.futuresHighOrLowMonitor(klines_list_1d, klines_list_15m);
+            klinesService.futuresHighOrLowMonitor(klines_list_1d, klines_list_15m);
 
         } catch (Exception e) {
             logger.error("分析" + pair + "交易对K线信息时出现异常", e);
