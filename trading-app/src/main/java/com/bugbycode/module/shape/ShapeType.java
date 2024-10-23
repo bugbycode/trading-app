87693c40ada3ba97f5c63d5a512e2fe8ed65bd4f
bugbycode <bugbycode@gmail.com>
2024-10-24 00:04:25 +0800

    修改绘图泛型名称

--- trading-app/src/main/java/com/bugbycode/module/shape/ShapeType.java
+++ trading-app/src/main/java/com/bugbycode/module/shape/ShapeType.java
@@ -8,7 +8,7 @@ public enum ShapeType {
 	/**
 	 * 趋势线
 	 */
-	TREND_LINE("LineToolTrendLine","trend_line","趋势线"),
+	LINE_TOOL_TREND_LINE("LineToolTrendLine","trend_line","趋势线"),
 	
 	/**
 	 * 射线
@@ -18,47 +18,47 @@ public enum ShapeType {
 	/**
 	 * 平行通道
 	 */
-	LineToolParallelChannel("LineToolParallelChannel","trend_line","平行通道"),
+	LINE_TOOL_PARALLEL_CHANNEL("LineToolParallelChannel","trend_line","平行通道"),
 	
 	/**
 	 * 水平射线
 	 */
-	LineToolHorzRay("LineToolHorzRay","horizontal_ray","水平射线"),
+	LINE_TOOL_HORZ_RAY("LineToolHorzRay","horizontal_ray","水平射线"),
 	
 	/**
 	 * 三角形
 	 */
-	LineToolTrianglePattern("LineToolTrianglePattern","triangle_pattern","三角形"),
+	LINE_TOOL_TRIANGLE_PATTERN("LineToolTrianglePattern","triangle_pattern","三角形"),
 	
 	/**
 	 * 盘整区
 	 */
-	LineToolRectangle("LineToolRectangle","rectangle","盘整区"),
+	LINE_TOOL_RECTANGLE("LineToolRectangle","rectangle","盘整区"),
 	
 	/**
 	 * 斐波那契回撤
 	 */
-	LineToolFibRetracement("LineToolFibRetracement","fib_retracement","斐波那契回撤"),
+	LINE_TOOL_FIB_RETRACEMENT("LineToolFibRetracement","fib_retracement","斐波那契回撤"),
 	
 	/**
 	 * XABCD
 	 */
-	LineTool5PointsPattern("LineTool5PointsPattern","xabcd_pattern","XABCD"),
+	LINE_TOOL_5POINTS_PATTERN("LineTool5PointsPattern","xabcd_pattern","XABCD"),
 	
 	/**
 	 * 做多交易计划
 	 */
-	LineToolRiskRewardLong("LineToolRiskRewardLong","long_position","做多交易计划"),
+	LINE_TOOL_RISK_REWARD_LONG("LineToolRiskRewardLong","long_position","做多交易计划"),
 	
 	/**
 	 * 做空交易计划
 	 */
-	LineToolRiskRewardShort("LineToolRiskRewardShort","short_position","做空交易计划"),
+	LINE_TOOL_RISK_REWARD_SHORT("LineToolRiskRewardShort","short_position","做空交易计划"),
 	
 	/**
 	 * 成交量密集分布
 	 */
-	LineToolFixedRangeVolumeProfile("LineToolFixedRangeVolumeProfile","fixed_range_volume_profile","成交量密集分布"),
+	LINE_TOOL_FIXED_RANGE_VOLUME_PROFILE("LineToolFixedRangeVolumeProfile","fixed_range_volume_profile","成交量密集分布"),
 	
 	UNKNOWN("Unknown","unknown","未知类型"),
 	;
