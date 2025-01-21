f1c55befea756ad309569b56fe6486bcdceeb1e5
bugbycode <bugbycode@gmail.com>
2025-01-21 21:39:50 +0800

    修改交易风格泛型

--- trading-app/src/main/java/com/bugbycode/module/TradeStyle.java
+++ trading-app/src/main/java/com/bugbycode/module/TradeStyle.java
@@ -8,23 +8,30 @@ public enum TradeStyle {
 	/**
 	 * 保守
 	 */
-	CONSERVATIVE(0),
+	CONSERVATIVE(0,"保守"),
 	
 	/**
 	 * 激进
 	 */
-	RADICAL(1);
+	RADICAL(1,"激进");
 	
 	private int value;
 	
-	private TradeStyle(int value) {
+	private String memo;
+	
+	private TradeStyle(int value, String memo) {
 		this.value = value;
+		this.memo = memo;
 	}
 	
 	public int getValue() {
 		return this.value;
 	}
 	
+	public String getMemo() {
+		return memo;
+	}
+
 	public static TradeStyle valueOf(int value) {
 		return value == 0 ? CONSERVATIVE : RADICAL;
 	}
