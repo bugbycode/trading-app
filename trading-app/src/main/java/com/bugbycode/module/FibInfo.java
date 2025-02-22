283af32e8a849df878338b57b73a3e5d291a0fae
bugbycode <bugbycode@gmail.com>
2025-02-22 11:40:18 +0800

    修改止三级斐波那契回撤盈点位

--- trading-app/src/main/java/com/bugbycode/module/FibInfo.java
+++ trading-app/src/main/java/com/bugbycode/module/FibInfo.java
@@ -19,7 +19,7 @@ public class FibInfo {
 	 */
 	public FibCode getTakeProfit_v7(FibCode code) {
 		FibCode takeProfit = FibCode.FIB382;
-		
+		/*
 		if(level == FibLevel.LEVEL_3) {
 			if(code == FibCode.FIB4_618) {//4.618 -> 2.618
 				takeProfit = FibCode.FIB2_618;
@@ -40,7 +40,7 @@ public class FibInfo {
 			} else if(code == FibCode.FIB382) { // 0.382 -> 0.236
 				takeProfit = FibCode.FIB236;
 			}
-		} else {
+		} else {*/
 			if(code == FibCode.FIB4_618) {//4.618 -> 2.618
 				takeProfit = FibCode.FIB2_618;
 			} else if(code == FibCode.FIB2_618) { // 2.618 -> 2
@@ -60,7 +60,7 @@ public class FibInfo {
 			} else if(code == FibCode.FIB382) { // 0.382 -> 0.236
 				takeProfit = FibCode.FIB236;
 			}
-		}
+		//}
 		
 		return takeProfit;
 	}
