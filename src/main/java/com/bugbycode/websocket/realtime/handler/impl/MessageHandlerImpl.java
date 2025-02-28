b1035e160b45eeae9a565be9c57d12148a46e2df
bugbycode <bugbycode@gmail.com>
2025-02-28 23:49:00 +0800

    修改日志输出级别

--- src/main/java/com/bugbycode/websocket/realtime/handler/impl/MessageHandlerImpl.java
+++ src/main/java/com/bugbycode/websocket/realtime/handler/impl/MessageHandlerImpl.java
@@ -40,7 +40,7 @@ public class MessageHandlerImpl implements MessageHandler{
 		
 		if(finish && client.putFinishPair(kline.getPair())) {
 			
-			logger.info(kline);
+			logger.debug(kline);
 			
 			//15分钟k线分析
 			long count = klinesRepository.count(kline.getPair(), Inerval.INERVAL_1D.getDescption());
