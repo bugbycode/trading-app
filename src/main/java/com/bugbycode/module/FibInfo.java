c5c105ac48e9e5dd43ea6a5b9aa168737567e9fd
bugbycode <bugbycode@gmail.com>
2025-03-14 12:42:04 +0800

    启用1.272回撤点

--- src/main/java/com/bugbycode/module/FibInfo.java
+++ src/main/java/com/bugbycode/module/FibInfo.java
@@ -90,7 +90,7 @@ public class FibInfo {
 			takeProfit = FibCode.FIB2_618;
 		} else if(code == FibCode.FIB2_618) { // 2.618 - 1.618
 			takeProfit = FibCode.FIB1_618;
-		} else if(code == FibCode.FIB2 || code == FibCode.FIB1_618) { // 2/1.618 - 1
+		} else if(code == FibCode.FIB2 || code == FibCode.FIB1_618 || code == FibCode.FIB1_272) { // 2/1.618/1.272 - 1
 			takeProfit = FibCode.FIB1;
 		} else if(code == FibCode.FIB1) { // 1 -> 0.5
 			takeProfit = FibCode.FIB5;
