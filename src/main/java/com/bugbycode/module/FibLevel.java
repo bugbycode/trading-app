24bc0a433b82dbbb3a9925533935950be719c434
bugbycode <bugbycode@gmail.com>
2025-03-18 18:06:09 +0800

    修改开仓点位

--- src/main/java/com/bugbycode/module/FibLevel.java
+++ src/main/java/com/bugbycode/module/FibLevel.java
@@ -5,19 +5,10 @@ package com.bugbycode.module;
  */
 public enum FibLevel {
 
-	/**
-	 * 震荡行情
-	 */
-	LEVEL_1(1,"Lv1",FibCode.FIB618),
+	LEVEL_1(1,"Lv1",FibCode.FIB382),
 	
-	/**
-	 * 多头走势
-	 */
-	LEVEL_2(2,"Lv2",FibCode.FIB618),
+	LEVEL_2(2,"Lv2",FibCode.FIB5),
 	
-	/**
-	 * 空头走势
-	 */
 	LEVEL_3(3,"Lv3",FibCode.FIB618),
 	
 	LEVEL_4(3,"Lv4",FibCode.FIB618);
