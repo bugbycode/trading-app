eb931d64d47029a735fab89211875f2af83d5929
bugbycode <bugbycode@gmail.com>
2025-03-19 14:20:18 +0800

    修改开仓点位

--- src/main/java/com/bugbycode/module/FibInfo.java
+++ src/main/java/com/bugbycode/module/FibInfo.java
@@ -204,7 +204,7 @@ public class FibInfo {
 		QuotationMode mode = this.getQuotationMode();
 		boolean result = false;
 		if(level == FibLevel.LEVEL_1 
-				&& code.lte(FibCode.FIB1) && code.gte(FibCode.FIB382)) {//震荡行情 1 ~ 0.382
+				&& code.lte(FibCode.FIB1) && code.gte(FibCode.FIB618)) {//震荡行情 1 ~ 0.618
 			result = true;
 		} else if(level == FibLevel.LEVEL_2 && mode == QuotationMode.LONG
 				 && code.lte(FibCode.FIB1) && code.gte(FibCode.FIB618)) {//多头行情做多 1 ~ 0.618
