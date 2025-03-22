1bd672672c20b5b060683eb2c6625a6e9d932891
bugbycode <bugbycode@gmail.com>
2025-03-23 01:27:47 +0800

    停用指数均线指标

--- src/main/java/com/bugbycode/trading_app/task/sync/work/AnalysisKlinesTask.java
+++ src/main/java/com/bugbycode/trading_app/task/sync/work/AnalysisKlinesTask.java
@@ -80,7 +80,7 @@ public class AnalysisKlinesTask implements Runnable{
             klinesService.futuresHighOrLowMonitor(klines_list_1d, klines_list_15m);
             
             //指数均线
-            klinesService.futuresEmaRiseAndFallMonitor(klines_list_15m);
+            //klinesService.futuresEmaRiseAndFallMonitor(klines_list_15m);
 
         } catch (Exception e) {
             logger.error("分析" + pair + "交易对K线信息时出现异常", e);
