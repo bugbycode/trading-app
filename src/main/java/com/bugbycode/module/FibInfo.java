05d68a666c6d2dbc6eb136b1cbad07fa9bc1aa93
bugbycode <bugbycode@gmail.com>
2025-06-30 10:21:01 +0800

    修改斐波那契回撤指标

--- src/main/java/com/bugbycode/module/FibInfo.java
+++ src/main/java/com/bugbycode/module/FibInfo.java
@@ -164,8 +164,8 @@ public class FibInfo {
 			takeProfit = FibCode.FIB1;
 		} else if(code == FibCode.FIB1_618) { // 1.618 - 1
 			takeProfit = FibCode.FIB1;
-		} else if(code == FibCode.FIB1_272) { // 1.272 - 1
-			takeProfit = FibCode.FIB1;
+		} else if(code == FibCode.FIB1_272) { // 1.272 - 0.786
+			takeProfit = FibCode.FIB786;
 		} else if(code == FibCode.FIB1) { // 1 -> 0.5
 			takeProfit = FibCode.FIB5;
 		} else if(code == FibCode.FIB786) { // 0.786 -> 0.382
