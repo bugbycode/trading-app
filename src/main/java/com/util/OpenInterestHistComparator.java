706112a2be7cbfae6613d86d87f4185d6e7e3c37
bugbycode <bugbycode@gmail.com>
2025-07-19 12:34:12 +0800

    修改交易对优先级排序方式

--- src/main/java/com/util/OpenInterestHistComparator.java
+++ src/main/java/com/util/OpenInterestHistComparator.java
@@ -6,7 +6,7 @@ import com.bugbycode.module.SortType;
 import com.bugbycode.module.open_interest.OpenInterestHist;
 
 /**
- * 合约持仓量排序工具类 按照SumOpenInterestValue（持仓总价值）来排序
+ * 合约持仓量排序工具类 按照tradeNumber（每分钟成交笔数）来排序
  */
 public class OpenInterestHistComparator implements Comparator<OpenInterestHist>{
 
@@ -21,8 +21,8 @@ public class OpenInterestHistComparator implements Comparator<OpenInterestHist>{
 		
 		int result = 0;
 		
-		double v1 = Double.valueOf(o1.getSumOpenInterestValue());
-		double v2 = Double.valueOf(o2.getSumOpenInterestValue());
+		double v1 = Double.valueOf(o1.getTradeNumber());
+		double v2 = Double.valueOf(o2.getTradeNumber());
 		if(type == SortType.DESC) {
 			if(v1 > v2) {
 				result = -1;
