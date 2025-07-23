3368e40ed6564c91bacc2111d63660228b241b7b
bugbycode <bugbycode@gmail.com>
2025-07-23 17:48:08 +0800

    回滚修改

--- src/main/java/com/util/FibInfoFactory.java
+++ src/main/java/com/util/FibInfoFactory.java
@@ -219,11 +219,11 @@ public class FibInfoFactory {
 	}
 	
 	private boolean verifyHigh(Klines k) {
-		return k.getEma7() > k.getEma25() && k.getEma25() > 0 && k.getMacd() > 0;
+		return k.getEma7() > k.getEma25() && k.getEma25() > 0;
 	}
 	
 	private boolean verifyLow(Klines k) {
-		return k.getEma7() < k.getEma25() && k.getEma25() > 0 && k.getMacd() < 0;
+		return k.getEma7() < k.getEma25() && k.getEma25() > 0;
 	}
 	
 	public FibInfo getFibInfo() {
