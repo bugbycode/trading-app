2e047425d0aba97419148dccd9287705f7382ba6
bugbycode <bugbycode@gmail.com>
2025-08-25 10:43:54 +0800

    订单类型枚举信息新增注释信息

--- src/main/java/com/bugbycode/module/trading/Type.java
+++ src/main/java/com/bugbycode/module/trading/Type.java
@@ -4,12 +4,40 @@ package com.bugbycode.module.trading;
  * 	订单类型 LIMIT, MARKET, STOP, TAKE_PROFIT, STOP_MARKET, TAKE_PROFIT_MARKET, TRAILING_STOP_MARKET
  */
 public enum Type {
-	LIMIT("LIMIT"), 
+	
+	/**
+	 * 限价单
+	 */
+	LIMIT("LIMIT"),
+	
+	/**
+	 * 市价单
+	 */
 	MARKET("MARKET"), 
+	
+	/**
+	 * 止损限价单
+	 */
 	STOP("STOP"), 
+	
+	/**
+	 * 止盈限价单
+	 */
 	TAKE_PROFIT("TAKE_PROFIT"), 
+	
+	/**
+	 * 止损市价单
+	 */
 	STOP_MARKET("STOP_MARKET"), 
+	
+	/**
+	 * 止盈市价单
+	 */
 	TAKE_PROFIT_MARKET("TAKE_PROFIT_MARKET"), 
+	
+	/**
+	 * 跟踪止损单
+	 */
 	TRAILING_STOP_MARKET("TRAILING_STOP_MARKET");
 
 	private String type;
