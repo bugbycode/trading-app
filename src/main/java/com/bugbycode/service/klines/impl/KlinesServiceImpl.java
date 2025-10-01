4e1711f6a751016a2daccc387d31f23c7a9399d6
bugbycode <bugbycode@gmail.com>
2025-10-01 12:25:42 +0800

    修改斐波那契回撤指标

--- src/main/java/com/bugbycode/service/klines/impl/KlinesServiceImpl.java
+++ src/main/java/com/bugbycode/service/klines/impl/KlinesServiceImpl.java
@@ -20,7 +20,7 @@ import com.bugbycode.config.AppConfig;
 import com.bugbycode.factory.ema.EmaTradingFactory;
 import com.bugbycode.factory.ema.impl.EmaTradingFactoryImpl;
 import com.bugbycode.factory.fibInfo.FibInfoFactory;
-import com.bugbycode.factory.fibInfo.impl.FibInfoFactoryImpl_v4;
+import com.bugbycode.factory.fibInfo.impl.FibInfoFactoryImpl_v7;
 import com.bugbycode.factory.priceAction.PriceActionFactory;
 import com.bugbycode.factory.priceAction.impl.PriceActionFactoryImpl;
 import com.bugbycode.module.BreakthroughTradeStatus;
@@ -452,7 +452,8 @@ public class KlinesServiceImpl implements KlinesService {
 			
 			int offset = fibInfo.getFibCodeIndex(code);
 			
-			if( (PriceUtil.isLong(price, klinesList_hit) || PriceUtil.isLong_v3(price, klinesList_hit))
+			if( //(PriceUtil.isLong(price, klinesList_hit) || PriceUtil.isLong_v3(price, klinesList_hit))
+					PriceUtil.isLong_v2(price, klinesList_hit)
 					&& !PriceUtil.isObsoleteLong(afterLowKlines, openPrices, index)
 					&& !PriceUtil.isTraded(price, fibInfo)
 					) {
@@ -549,7 +550,8 @@ public class KlinesServiceImpl implements KlinesService {
 			
 			int offset = fibInfo.getFibCodeIndex(code);
 			
-			if( (PriceUtil.isShort(price, klinesList_hit) || PriceUtil.isShort_v3(price, klinesList_hit))
+			if( //(PriceUtil.isShort(price, klinesList_hit) || PriceUtil.isShort_v3(price, klinesList_hit))
+					PriceUtil.isShort_v2(price, klinesList_hit)
 					&& !PriceUtil.isObsoleteShort(afterHighKlines, openPrices, index)
 					&& !PriceUtil.isTraded(price, fibInfo)
 					) {
@@ -1294,7 +1296,7 @@ public class KlinesServiceImpl implements KlinesService {
 	@Override
 	public void futuresFibMonitor(List<Klines> list_1d, List<Klines> list_4h, List<Klines> list_1h,  List<Klines> list_15m) {
 		
-		FibInfoFactory factory = new FibInfoFactoryImpl_v4(list_1h, list_1h, list_15m);
+		FibInfoFactory factory = new FibInfoFactoryImpl_v7(list_4h, list_4h, list_15m);
 		
 		FibInfo fibInfo = factory.getFibInfo();
 		
