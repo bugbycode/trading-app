3cc3f642dda7a0429578039ecbd82295589ce0f2
bugbycode <bugbycode@gmail.com>
2025-10-30 12:10:24 +0800

    修改斐波那契回撤指标

--- src/main/java/com/bugbycode/service/klines/impl/KlinesServiceImpl.java
+++ src/main/java/com/bugbycode/service/klines/impl/KlinesServiceImpl.java
@@ -1323,7 +1323,7 @@ public class KlinesServiceImpl implements KlinesService {
 	@Override
 	public void futuresFibMonitor(List<Klines> list_1d, List<Klines> list_4h, List<Klines> list_1h,  List<Klines> list_15m) {
 		
-		FibInfoFactory factory = new FibInfoFactoryImpl(list_1h, list_4h, list_15m);
+		FibInfoFactory factory = new FibInfoFactoryImpl(list_15m, list_1h, list_15m);
 		
 		FibInfo fibInfo = factory.getFibInfo();
 		
