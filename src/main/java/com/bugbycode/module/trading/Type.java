2fb1117e7002f5d2d267aa5872566280bff8cb2e
bugbycode <bugbycode@gmail.com>
2026-01-18 14:14:05 +0800

    修改订单类型描述

--- src/main/java/com/bugbycode/module/trading/Type.java
+++ src/main/java/com/bugbycode/module/trading/Type.java
@@ -6,39 +6,39 @@ package com.bugbycode.module.trading;
 public enum Type {
 	
 	/**
-	 * 限价单
+	 * 限价订单
 	 */
-	LIMIT("LIMIT", "限价单"),
+	LIMIT("LIMIT", "限价订单"),
 	
 	/**
-	 * 市价单
+	 * 市价订单
 	 */
-	MARKET("MARKET", "市价单"), 
+	MARKET("MARKET", "市价订单"), 
 	
 	/**
-	 * 止损限价单
+	 * 限价止损订单
 	 */
-	STOP("STOP", "止损限价单"), 
+	STOP("STOP", "限价止损订单"), 
 	
 	/**
-	 * 止盈限价单
+	 * 限价止盈订单
 	 */
-	TAKE_PROFIT("TAKE_PROFIT", "止盈限价单"), 
+	TAKE_PROFIT("TAKE_PROFIT", "限价止盈订单"), 
 	
 	/**
-	 * 止损市价单
+	 * 市价止损订单
 	 */
-	STOP_MARKET("STOP_MARKET", "止损市价单"), 
+	STOP_MARKET("STOP_MARKET", "市价止损订单"), 
 	
 	/**
-	 * 止盈市价单
+	 * 市价止盈订单
 	 */
-	TAKE_PROFIT_MARKET("TAKE_PROFIT_MARKET", "止盈市价单"), 
+	TAKE_PROFIT_MARKET("TAKE_PROFIT_MARKET", "市价止盈订单"), 
 	
 	/**
-	 * 跟踪止损单
+	 * 跟踪委托订单
 	 */
-	TRAILING_STOP_MARKET("TRAILING_STOP_MARKET", "跟踪止损单");
+	TRAILING_STOP_MARKET("TRAILING_STOP_MARKET", "跟踪委托订单");
 
 	private String type;
 	
