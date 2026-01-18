56b33b6b6c65559b46dc83e3c20ec1220bfef8a4
bugbycode <bugbycode@gmail.com>
2026-01-18 13:25:27 +0800

    禁用价格行为和盘整区分析

--- src/main/java/com/bugbycode/trading_app/task/sync/work/AnalysisKlinesTask.java
+++ src/main/java/com/bugbycode/trading_app/task/sync/work/AnalysisKlinesTask.java
@@ -141,13 +141,13 @@ public class AnalysisKlinesTask implements Runnable{
             klinesService.futuresFibMonitor(klines_list_1d, klines_list_4h_db, klines_list_1h_db, klines_list_15m);
             
             //盘整区分析
-            klinesService.consolidationAreaMonitor(klines_list_1h_db, klines_list_15m);
+            //klinesService.consolidationAreaMonitor(klines_list_1h_db, klines_list_15m);
             
             //指数均线
             //klinesService.futuresEmaRiseAndFallMonitor(klines_list_1h_db, klines_list_15m);
             
             //价格行为分析
-            klinesService.futuresPriceAction(klines_list_1h_db, klines_list_15m);
+            //klinesService.futuresPriceAction(klines_list_1h_db, klines_list_15m);
             
             //量价分析
             //klinesService.volumeMonitor(klines_list_1d, klines_list_4h_db, klines_list_1h, klines_list_15m);
