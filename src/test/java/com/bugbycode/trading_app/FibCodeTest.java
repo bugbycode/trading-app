e6f346411bd6c25d5b6b29c59f00174c75231492
bugbycode <bugbycode@gmail.com>
2026-02-01 11:41:59 +0800

    单元测试代码

--- src/test/java/com/bugbycode/trading_app/FibCodeTest.java
+++ src/test/java/com/bugbycode/trading_app/FibCodeTest.java
@@ -3,14 +3,12 @@ package com.bugbycode.trading_app;
 import com.bugbycode.module.FibCode;
 import com.bugbycode.module.FibInfo;
 import com.bugbycode.module.FibLevel;
-import com.bugbycode.module.TradeFrequency;
 
 public class FibCodeTest {
 
 	public static void main(String[] args) {
 		
 		FibInfo fibInfo = new FibInfo(1, 100, 2, FibLevel.LEVEL_0);
-		fibInfo.setTradeFrequency(TradeFrequency.HIGH);
 		
 		FibCode[] codes = FibCode.values();
 		
