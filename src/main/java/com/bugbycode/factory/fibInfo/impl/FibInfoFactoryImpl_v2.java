ed3b5f36d7ca9727cfa584c141202c7072ed0898
bugbycode <bugbycode@gmail.com>
2026-03-09 19:31:58 +0800

    修复已知BUG

--- src/main/java/com/bugbycode/factory/fibInfo/impl/FibInfoFactoryImpl_v2.java
+++ src/main/java/com/bugbycode/factory/fibInfo/impl/FibInfoFactoryImpl_v2.java
@@ -242,11 +242,11 @@ public class FibInfoFactoryImpl_v2 implements FibInfoFactory {
 	}
 	
 	private boolean verifyHigh(Klines k) {
-		return k.getMacd() > 0 && k.getDea() > 0;
+		return k.getMacd() > 0;
 	}
 	
 	private boolean verifyLow(Klines k) {
-		return k.getMacd() < 0 && k.getDea() < 0;
+		return k.getMacd() < 0;
 	}
 	
 	private void addPrices(OpenPrice price) {
