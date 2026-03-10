75124c1f0480b4fa53e9a5860fb0c7dfcf606850
bugbycode <bugbycode@gmail.com>
2026-03-10 23:44:07 +0800

    修改盘整区指标

--- src/main/java/com/bugbycode/factory/area/impl/AreaFactoryImpl_v2.java
+++ src/main/java/com/bugbycode/factory/area/impl/AreaFactoryImpl_v2.java
@@ -8,7 +8,7 @@ import org.springframework.util.CollectionUtils;
 import com.bugbycode.factory.area.AreaFactory;
 import com.bugbycode.module.FibCode;
 import com.bugbycode.module.Klines;
-import com.bugbycode.module.MarketSentiment;
+import com.bugbycode.module.PriceActionInfo_v2;
 import com.bugbycode.module.SortType;
 import com.bugbycode.module.price.OpenPrice;
 import com.bugbycode.module.price.impl.OpenPriceDetails;
@@ -56,71 +56,61 @@ public class AreaFactoryImpl_v2 implements AreaFactory {
 		
 		ps = PositionSide.DEFAULT;
 		
-		Klines current = null;
-		Klines parent = null;
+		PriceActionInfo_v2 info = null;
 		
 		for(int index = list.size() - 1; index > 0; index--) {
-			current = list.get(index);
-			parent = list.get(index - 1);
+			Klines current = list.get(index);
+			Klines parent = list.get(index - 1);
 			
-			if(PriceUtil.isRise_v4(current, parent)) {
+			if(PriceUtil.verifyPowerful_v27(current, parent)) {
 				ps = PositionSide.LONG;
+				info = new PriceActionInfo_v2(current, parent, null);
 				break;
-			} else if(PriceUtil.isFall_v4(current, parent)) {
+			} else if(PriceUtil.verifyDecliningPrice_v27(current, parent)) {
 				ps = PositionSide.SHORT;
+				info = new PriceActionInfo_v2(current, parent, null);
 				break;
 			}
 			
-			current = null;
-			parent = null;
 		}
 		
-		if(current == null || parent == null) {
+		if(info == null) {
 			return;
 		}
 		
+		Klines last_15m = PriceUtil.getLastKlines(list_15m);
+		double stopLoss = isLong() ? last_15m.getLowPriceDoubleValue() : last_15m.getHighPriceDoubleValue();
+		
 		FibCode openCode = FibCode.FIB618;
-		List<Klines> data = new ArrayList<Klines>();
-		data.add(current);
-		data.add(parent);
-		
-		MarketSentiment ms = new MarketSentiment(data);
-		
-		double closePrice = current.getClosePriceDoubleValue();
-		double parentBodyHighPrice = parent.getBodyHighPriceDoubleValue();
-		double parentBodyLowPrice = parent.getBodyLowPriceDoubleValue();
-		double bodyLowPrice = ms.getMinBodyLowPrice();
-		double bodyHighPrice = ms.getMaxBodyHighPrice();
-		double lowPrice = ms.getLowPrice();
-		double highPrice = ms.getHighPrice();
-		
+		double firstTakeProfit;
+		double secondTakeProfit;
 		if(ps == PositionSide.LONG) {//做多
 			
-			addPrices(new OpenPriceDetails(openCode, closePrice, parentBodyHighPrice, highPrice, highPrice));//收盘价
-			addPrices(new OpenPriceDetails(openCode, parentBodyHighPrice, bodyLowPrice, bodyHighPrice, highPrice));//前一根k线最高实体价
-			addPrices(new OpenPriceDetails(openCode, bodyLowPrice, lowPrice, bodyHighPrice, highPrice));//最低实体价
-		
-			if(current.getBodyHighPriceDoubleValue() > parent.getHighPriceDoubleValue()) {//当前K线实体价格高于前一根K线最高价情况
-				addPrices(new OpenPriceDetails(openCode, parent.getHighPriceDoubleValue(), parentBodyHighPrice, bodyHighPrice, highPrice));
-			}
+			firstTakeProfit = info.getCurrent().getBodyHighPriceDoubleValue();
+			secondTakeProfit = info.getCurrent().getHighPriceDoubleValue();
+			
+			addPrices(new OpenPriceDetails(openCode, info.getParent().getHighPriceDoubleValue(), stopLoss, firstTakeProfit, secondTakeProfit));
+			addPrices(new OpenPriceDetails(openCode, info.getParent().getBodyHighPriceDoubleValue(), stopLoss, firstTakeProfit, secondTakeProfit));
+			addPrices(new OpenPriceDetails(openCode, info.getParent().getBodyLowPriceDoubleValue(), stopLoss, firstTakeProfit, secondTakeProfit));
+			addPrices(new OpenPriceDetails(openCode, info.getLow().getLowPriceDoubleValue(), stopLoss, firstTakeProfit, secondTakeProfit));
 			
 			this.openPrices.sort(new PriceComparator(SortType.DESC));
 			
 		} else {//做空
 			
-			addPrices(new OpenPriceDetails(openCode, closePrice, parentBodyLowPrice, lowPrice, lowPrice));//收盘价
-			addPrices(new OpenPriceDetails(openCode, parentBodyLowPrice, bodyHighPrice, bodyLowPrice, lowPrice));//前一根K线最低实体价
-			addPrices(new OpenPriceDetails(openCode, bodyHighPrice, highPrice, bodyLowPrice, lowPrice)); //最高实体价
+			firstTakeProfit = info.getCurrent().getBodyLowPriceDoubleValue();
+			secondTakeProfit = info.getCurrent().getLowPriceDoubleValue();
 			
-			if(current.getBodyLowPriceDoubleValue() < parent.getLowPriceDoubleValue()) {//当前k线实体价格低于前一根k线最低价情况
-				addPrices(new OpenPriceDetails(openCode, parent.getLowPriceDoubleValue(), parentBodyLowPrice, bodyLowPrice, lowPrice));
-			}
+			addPrices(new OpenPriceDetails(openCode, info.getParent().getLowPriceDoubleValue(), stopLoss, firstTakeProfit, secondTakeProfit));
+			addPrices(new OpenPriceDetails(openCode, info.getParent().getBodyLowPriceDoubleValue(), stopLoss, firstTakeProfit, secondTakeProfit));
+			addPrices(new OpenPriceDetails(openCode, info.getParent().getBodyHighPriceDoubleValue(), stopLoss, firstTakeProfit, secondTakeProfit));
+			addPrices(new OpenPriceDetails(openCode, info.getHigh().getHighPriceDoubleValue(), stopLoss, firstTakeProfit, secondTakeProfit));
 			
 			this.openPrices.sort(new PriceComparator(SortType.ASC));
 			
 		}
 		
-		Klines fibAfterFlag = PriceUtil.getAfterKlines(current, this.list_15m);
+		Klines fibAfterFlag = PriceUtil.getAfterKlines(info.getCurrent(), this.list_15m);
 		if(fibAfterFlag != null) {
 			this.fibAfterKlines.addAll(PriceUtil.subList(fibAfterFlag, this.list_15m));
 		}
