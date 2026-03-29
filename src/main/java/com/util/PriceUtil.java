e2ea95da6a2ad45c4128e2b8a58960d65cad6184
bugbycode <bugbycode@gmail.com>
2026-03-29 13:27:56 +0800

    启用价格行为分析

--- src/main/java/com/util/PriceUtil.java
+++ src/main/java/com/util/PriceUtil.java
@@ -2128,6 +2128,19 @@ public class PriceUtil {
 	}
 	
 	/**
+	 * 判断是否出现颓势 </br></br>
+	 * 
+	 * 1、当前k线收盘价小于ema7且前一根k线收盘价大于等于ema7
+	 * 
+	 * @param current 当前k线
+	 * @param parent 前一根k线
+	 * @return
+	 */
+	public static boolean verifyDecliningPrice_v29(Klines current, Klines parent) {
+		return current.getClosePriceDoubleValue() < current.getEma7() && parent.getClosePriceDoubleValue() >= parent.getEma7();
+	}
+	
+	/**
 	 * MACD 连续衰竭
 	 * @param current 当前k线
 	 * @param parent 前一根k线
@@ -2426,6 +2439,19 @@ public class PriceUtil {
 	 */
 	public static boolean verifyPowerful_v28(Klines current, Klines parent) {
 		return current.getClosePriceDoubleValue() >= parent.getOpenPriceDoubleValue();
+	}
+	
+	/**
+	 * 判断是否出现强势 </br></br>
+	 * 
+	 * 当前k线收盘价大于ema7且前一根k线收盘价小于等于ema7
+	 * 
+	 * @param current 当前k线
+	 * @param parent 前一根k线
+	 * @return
+	 */
+	public static boolean verifyPowerful_v29(Klines current, Klines parent) {
+		return current.getClosePriceDoubleValue() > current.getEma7() && parent.getClosePriceDoubleValue() <= parent.getEma7();
 	}
 	
 	/**
