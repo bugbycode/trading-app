a78556d08d88051b600baccdcd24b76b6401b928
bugbycode <bugbycode@gmail.com>
2026-04-14 20:51:47 +0800

    修复已知BUG

--- src/main/java/com/bugbycode/factory/fibInfo/impl/FibInfoFactoryImpl.java
+++ src/main/java/com/bugbycode/factory/fibInfo/impl/FibInfoFactoryImpl.java
@@ -209,8 +209,8 @@ public class FibInfoFactoryImpl implements FibInfoFactory {
 			
 			if(openCode.gt(FibCode.FIB0)) {
 				double fibValue = fibInfo.getFibValue(openCode);
-				for(int index = list_15m.size() - 1; index > 0; index--) {
-					Klines current = list_15m.get(index);
+				for(int index = list.size() - 1; index > 0; index--) {
+					Klines current = list.get(index);
 					if(current.lte(end)) {
 						break;
 					}
