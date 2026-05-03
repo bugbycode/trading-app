0755b5919e5e220db80e1ce58de655973c68b817
bugbycode <bugbycode@gmail.com>
2026-05-03 18:38:37 +0800

    修复已知BUG

--- src/main/java/com/bugbycode/module/FibInfo.java
+++ src/main/java/com/bugbycode/module/FibInfo.java
@@ -161,7 +161,7 @@ public class FibInfo {
 	 */
 	public FibCode getTakeProfit_v2(FibCode code) {
 		FibCode takeProfit = FibCode.FIB0;
-		if(code == FibCode.FIB4_618) { // 4.618 - 2
+		/*if(code == FibCode.FIB4_618) { // 4.618 - 2
 			takeProfit = FibCode.FIB2;
 		} else if(code == FibCode.FIB3_618) { // 3.618 - 1.618
 			takeProfit = FibCode.FIB1_618;
@@ -170,6 +170,24 @@ public class FibInfo {
 		} else if(code == FibCode.FIB2) { // 2 - 1
 			takeProfit = FibCode.FIB1;
 		} else if(code == FibCode.FIB1_618) { // 1.618 - 1
+			takeProfit = FibCode.FIB1;
+		} else if(code == FibCode.FIB1_272) { // 1.272 - 0.786
+			takeProfit = FibCode.FIB786;
+		} else if(code == FibCode.FIB1) { // 1 -> 0.5
+			takeProfit = FibCode.FIB5;
+		} else if(code == FibCode.FIB786) { // 0.786 -> 0.382
+			takeProfit = FibCode.FIB382;
+		} else if(code == FibCode.FIB618) { // 0.618 -> 0.236
+			takeProfit = FibCode.FIB236;
+		} else if(code == FibCode.FIB5) { // 0.5 -> 0.236
+			takeProfit = FibCode.FIB236;
+		} else if(code == FibCode.FIB382) { // 0.382 -> 0
+			takeProfit = FibCode.FIB0;
+		} else if(code == FibCode.FIB236) { // 0.236 -> 0
+			takeProfit = FibCode.FIB0;
+		}*/
+		
+		if(code.gte(FibCode.FIB1_618)) { // 4.618 ~ 1.618 - 1
 			takeProfit = FibCode.FIB1;
 		} else if(code == FibCode.FIB1_272) { // 1.272 - 0.786
 			takeProfit = FibCode.FIB786;
