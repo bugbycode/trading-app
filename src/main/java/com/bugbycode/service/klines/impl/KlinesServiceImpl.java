5e9bb25f0a7ef771c850489d0c786ec7dcb6cb3c
bugbycode <bugbycode@gmail.com>
2026-05-05 12:18:26 +0800

    修复已知BUG

--- src/main/java/com/bugbycode/service/klines/impl/KlinesServiceImpl.java
+++ src/main/java/com/bugbycode/service/klines/impl/KlinesServiceImpl.java
@@ -1125,7 +1125,7 @@ public class KlinesServiceImpl implements KlinesService {
 	public void consolidationAreaMonitor(List<Klines> list_1d, List<Klines> list_4h, List<Klines> list_1h,  List<Klines> list_15m) {
 		
 		AreaFactory[] factories = {
-					new AreaFactoryImpl(list_1h, list_15m)
+					new AreaFactoryImpl(list_1d, list_15m)
 				};
 		
 		for(AreaFactory factory : factories) {
