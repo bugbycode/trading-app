68c9f4f8a11ecad62359dbc4a1480ac80e069798
bugbycode <bugbycode@gmail.com>
2026-05-12 17:22:57 +0800

    Revert "修复已知BUG"
    This reverts commit d4e0f1f76e4d43441552ab95bc0316d7db39684e.

--- src/main/java/com/bugbycode/factory/priceAction/impl/PriceActionFactoryImpl.java
+++ src/main/java/com/bugbycode/factory/priceAction/impl/PriceActionFactoryImpl.java
@@ -70,6 +70,7 @@ public class PriceActionFactoryImpl implements PriceActionFactory{
 		this.list_15m.sort(kc);
 		
 		PriceUtil.calculateMACD(list);
+		PriceUtil.calculateEMA_7_25_99(list);
 		
 		this.openPrices = new ArrayList<OpenPrice>();
 		this.fibAfterKlines = new ArrayList<Klines>();
@@ -227,19 +228,19 @@ public class PriceActionFactoryImpl implements PriceActionFactory{
 	}
 	
 	private boolean verifyLong(Klines k) {
-		return k.getMacd() < 0;
+		return k.getEma7() < k.getEma25() && k.getEma25() > 0;
 	}
 	
 	private boolean verifyShort(Klines k) {
-		return k.getMacd() > 0;
+		return k.getEma7() > k.getEma25() && k.getEma25() > 0;
 	}
 	
 	private boolean verifyHigh(Klines k) {
-		return k.getMacd() > 0;
+		return k.getEma7() > k.getEma25() && k.getEma25() > 0 && k.getMacd() > 0;
 	}
 	
 	private boolean verifyLow(Klines k) {
-		return k.getMacd() < 0;
+		return k.getEma7() < k.getEma25() && k.getEma25() > 0 && k.getMacd() < 0;
 	}
 	
 	private void addPrices(OpenPrice price) {
