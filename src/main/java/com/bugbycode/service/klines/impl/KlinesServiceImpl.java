677d64788e27de873090774d452e3a859a237b9c
bugbycode <bugbycode@gmail.com>
2026-05-26 00:02:20 +0800

    修改盘整区指标

--- src/main/java/com/bugbycode/service/klines/impl/KlinesServiceImpl.java
+++ src/main/java/com/bugbycode/service/klines/impl/KlinesServiceImpl.java
@@ -1166,7 +1166,7 @@ public class KlinesServiceImpl implements KlinesService {
 	public void consolidationAreaMonitor(List<Klines> list_1d, List<Klines> list_4h, List<Klines> list_1h,  List<Klines> list_15m) {
 		
 		AreaFactory[] factories = {
-					new AreaFactoryImpl(list_1h, list_15m)
+					new AreaFactoryImpl(list_4h, list_15m, list_15m)
 				};
 		
 		for(AreaFactory factory : factories) {
