23e6619dac0280c777010400f7c62ce00df8df92
bugbycode <bugbycode@gmail.com>
2026-05-26 14:33:22 +0800

    修复已知BUG

--- src/main/java/com/bugbycode/factory/priceAction/impl/PriceActionFactoryImpl.java
+++ src/main/java/com/bugbycode/factory/priceAction/impl/PriceActionFactoryImpl.java
@@ -264,7 +264,7 @@ public class PriceActionFactoryImpl implements PriceActionFactory{
 					
 					
 					
-					FibInfo stopLossFibInfo = new FibInfo(openPriceValue, secondTakeProfit, fibInfo.getDecimalPoint());
+					FibInfo stopLossFibInfo = new FibInfo(hitValue, secondTakeProfit, fibInfo.getDecimalPoint());
 					double stopLossValue = stopLossFibInfo.getFibValue(FibCode.FIB1_272);
 					
 					addPrices(new OpenPriceDetails(openCode, openPriceValue, stopLossValue, firstTakeProfit, secondTakeProfit, AutoTradeType.PRICE_ACTION, fibInfo));
