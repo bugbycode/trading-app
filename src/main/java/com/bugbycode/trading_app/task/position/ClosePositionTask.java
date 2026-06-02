d2a445e81930f44320fd92bdf8c30beb621191a6
bugbycode <bugbycode@gmail.com>
2026-06-03 01:28:28 +0800

    新增自动平仓定时任务

---
+++ src/main/java/com/bugbycode/trading_app/task/position/ClosePositionTask.java
@@ -0,0 +1,93 @@
+package com.bugbycode.trading_app.task.position;
+
+import java.util.Date;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.util.CollectionUtils;
+
+import com.bugbycode.binance.module.position.PositionInfo;
+import com.bugbycode.binance.trade.rest.BinanceRestTradeService;
+import com.bugbycode.binance.trade.websocket.BinanceWebsocketTradeService;
+import com.bugbycode.config.AppConfig;
+import com.bugbycode.exception.OrderPlaceException;
+import com.bugbycode.module.Result;
+import com.bugbycode.module.binance.AutoTrade;
+import com.bugbycode.module.binance.AutoTradeType;
+import com.bugbycode.module.binance.BinanceOrderInfo;
+import com.bugbycode.module.user.User;
+import com.bugbycode.repository.user.UserRepository;
+import com.bugbycode.trading_app.pool.WorkTaskPool;
+import com.bugbycode.trading_app.task.email.SendMailTask;
+import com.util.DateFormatUtil;
+
+/**
+ * 自动关闭现有持仓
+ */
+@Configuration
+@EnableScheduling
+public class ClosePositionTask {
+
+	private final Logger logger = LogManager.getLogger(ClosePositionTask.class);
+	
+	@Autowired
+	private BinanceWebsocketTradeService binanceWebsocketTradeService;
+	
+	@Autowired
+    private UserRepository userRepository;
+
+	@Autowired
+    private BinanceRestTradeService binanceRestTradeService;
+	
+	@Autowired
+	private WorkTaskPool emailWorkTaskPool;
+	
+	//每天上午七点五十分执行
+	@Scheduled(cron = "0 50 7 * * ?")
+	public void executeTask() {
+		
+		if(AppConfig.DEBUG) {
+			return;
+		}
+		
+		//查询所有启用墙头草策略用户
+		List<User> userList = userRepository.queryByAutoTrade(AutoTrade.OPEN, AutoTradeType.FENCE_SITTER);
+		if(!CollectionUtils.isEmpty(userList)) {
+			for(User u : userList) {
+				try {
+					String tradeUserEmail = u.getUsername();
+					String dateStr = DateFormatUtil.format(new Date());
+					String binanceApiKey = u.getBinanceApiKey();
+					String binanceSecretKey = u.getBinanceSecretKey();
+					List<PositionInfo> positionList = binanceRestTradeService.positionRisk_v3(binanceApiKey, binanceSecretKey, null);
+					for(PositionInfo info : positionList) {
+						Result<BinanceOrderInfo, RuntimeException> rs = binanceWebsocketTradeService.closePositionInfo(binanceApiKey, binanceSecretKey, info);
+						RuntimeException ex = rs.getErr();
+						if(ex == null) {
+							logger.info("{}仓位已平仓", info.getSymbol());
+						} else {
+							String title = "平仓" + info.getSymbol() + "仓位时出现异常";
+							String message = ex.getMessage();
+							if(ex instanceof OrderPlaceException) {
+								title = ((OrderPlaceException)ex).getTitle();
+							}
+							emailWorkTaskPool.add(new SendMailTask(u, title + " " + dateStr, message, tradeUserEmail, userRepository));
+						}
+						try {
+							Thread.sleep(500);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+				} catch (Exception e) {
+					logger.error(e.getMessage(), e);
+				}
+			}
+		}
+	}
+}
