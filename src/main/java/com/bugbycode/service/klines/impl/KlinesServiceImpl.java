51a66e44f0ca4f6b61836de0cd3e2f4ea0c83ed1
bugbycode <bugbycode@gmail.com>
2026-06-09 10:55:47 +0800

    修复已知BUG

--- src/main/java/com/bugbycode/service/klines/impl/KlinesServiceImpl.java
+++ src/main/java/com/bugbycode/service/klines/impl/KlinesServiceImpl.java
@@ -1072,7 +1072,7 @@ public class KlinesServiceImpl implements KlinesService {
 	public void futuresPriceAction(List<Klines> list_1d, List<Klines> list_4h, List<Klines> list_1h,  List<Klines> list_15m) {
 		
 		PriceActionFactory[] factorys = {
-				new PriceActionFactoryImpl(list_15m, list_15m, list_15m),
+				new PriceActionFactoryImpl(list_1h, list_1h, list_15m),
 		};
 		
 		for(PriceActionFactory factory : factorys) {
