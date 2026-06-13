8b0fb7d39ce999ddd0e6fd744b1348d7033fc4b4
bugbycode <bugbycode@gmail.com>
2026-06-13 11:40:22 +0800

    修复已知BUG

--- src/main/java/com/bugbycode/factory/fibInfo/impl/FibInfoFactoryImpl.java
+++ src/main/java/com/bugbycode/factory/fibInfo/impl/FibInfoFactoryImpl.java
@@ -251,11 +251,11 @@ public class FibInfoFactoryImpl implements FibInfoFactory {
 	}
 	
 	private boolean verifyLong(Klines k) {
-		return k.getDea() > 0;
+		return k.getMacd() < 0;
 	}
 	
 	private boolean verifyShort(Klines k) {
-		return k.getDea() < 0;
+		return k.getMacd() > 0;
 	}
 	
 	private boolean verifyHigh(Klines k) {
