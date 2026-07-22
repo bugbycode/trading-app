d263b802c817004490843a73c20e83ef730f12c4
bugbycode <bugbycode@gmail.com>
2026-07-22 21:06:57 +0800

    修复已知BUG

--- src/main/java/com/bugbycode/service/klines/impl/KlinesServiceImpl.java
+++ src/main/java/com/bugbycode/service/klines/impl/KlinesServiceImpl.java
@@ -1077,8 +1077,8 @@ public class KlinesServiceImpl implements KlinesService {
 	public void futuresFibMonitor(List<Klines> list_1d, List<Klines> list_4h, List<Klines> list_1h,  List<Klines> list_15m) {
 		
 		FibInfoFactory[] factorys = {
-				new FibInfoFactoryImpl(list_4h, list_4h, list_15m, TradeTrend.FOLLOW),
-				new FibInfoFactoryImpl(list_4h, list_4h, list_15m, TradeTrend.AGAINST)
+				new FibInfoFactoryImpl(list_1h, list_1h, list_15m, TradeTrend.FOLLOW),
+				new FibInfoFactoryImpl(list_1h, list_1h, list_15m, TradeTrend.AGAINST)
 		};
 		
 		for(FibInfoFactory factory : factorys) {
