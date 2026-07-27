b60eaeb983c0686283edf146f6bf0301dc2a05bd
bugbycode <bugbycode@gmail.com>
2026-07-27 11:27:06 +0800

    单元测试代码

--- src/test/java/com/bugbycode/trading_app/KlinesServiceTest.java
+++ src/test/java/com/bugbycode/trading_app/KlinesServiceTest.java
@@ -223,7 +223,7 @@ public class KlinesServiceTest {
 
     @Test
     public void testFibInfo(){
-        String pair = "ETHUSDT";
+        String pair = "DOGEUSDT";
         //List<Klines> list_1d = klinesRepository.findLastKlinesByPair(pair, Inerval.INERVAL_1D,1500);
         List<Klines> list_trend = klinesRepository.findLastKlinesByPair(pair, Inerval.INERVAL_1H,1500);
         List<Klines> list = klinesRepository.findLastKlinesByPair(pair, Inerval.INERVAL_1H, 1500);
@@ -235,7 +235,7 @@ public class KlinesServiceTest {
         
         //logger.info(klines_list_1h);
         
-        FibInfoFactory factory = new FibInfoFactoryImpl(list_trend, list, list_15m);
+        FibInfoFactory factory = new FibInfoFactoryImpl(list_trend, list, list_15m, PositionSide.SHORT);
         
         if(!(factory.isLong() || factory.isShort())) {
         	return;
