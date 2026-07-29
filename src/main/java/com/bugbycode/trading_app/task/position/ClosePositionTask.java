942b22a8bd5bd5c2ffcca5d8943be8022ff5a74f
bugbycode <bugbycode@gmail.com>
2026-07-29 23:04:14 +0800

    修复已知BUG

--- src/main/java/com/bugbycode/trading_app/task/position/ClosePositionTask.java
+++ src/main/java/com/bugbycode/trading_app/task/position/ClosePositionTask.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.util.CollectionUtils;
 
 import com.bugbycode.binance.module.position.PositionInfo;
 import com.bugbycode.binance.trade.websocket.BinanceWebsocketTradeService;
@@ -39,6 +40,8 @@ public class ClosePositionTask implements Runnable{
 	
 	private User user;
 	
+	private List<User> users;
+	
 	/**
 	 * 关闭用户持仓任务
 	 * @param pair 交易对
@@ -49,6 +52,18 @@ public class ClosePositionTask implements Runnable{
 		this.pair = pair;
 		this.ps = ps;
 		this.user = user;
+	}
+	
+	/**
+	 * 关闭用户持仓任务
+	 * @param pair 交易对
+	 * @param ps 要关闭的持仓方向 LONG/SHORT
+	 * @param users 相关用户
+	 */
+	public ClosePositionTask(String pair, PositionSide ps, List<User> users) {
+		this.pair = pair;
+		this.ps = ps;
+		this.users = users;
 	}
 	
 	/**
@@ -77,6 +92,8 @@ public class ClosePositionTask implements Runnable{
 				userList = userDetailsService.queryByAutoTrade(AutoTrade.OPEN, autoTradeType);
 			} else if(user != null) {
 				userList.add(user);
+			} else if(!CollectionUtils.isEmpty(users)) {
+				userList.addAll(users);
 			}
 			
 			for(User u : userList) {
