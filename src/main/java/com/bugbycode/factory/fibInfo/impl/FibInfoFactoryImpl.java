b0b7c8102cb96fb119a4697b5af6d6fdb76fa3c5
bugbycode <bugbycode@gmail.com>
2026-08-06 14:01:12 +0800

    修复已知BUG

--- src/main/java/com/bugbycode/factory/fibInfo/impl/FibInfoFactoryImpl.java
+++ src/main/java/com/bugbycode/factory/fibInfo/impl/FibInfoFactoryImpl.java
@@ -209,8 +209,16 @@ public class FibInfoFactoryImpl implements FibInfoFactory {
 			
 			FibCode takeProfitCode = FibCode.FIB618;
 			
-			if(openCode.gte(FibCode.FIB1) && tradeTrend == TradeTrend.AGAINST) {
+			if(openCode == FibCode.FIB1_272) {
+				takeProfitCode = FibCode.FIB236;
+			} else if(openCode == FibCode.FIB1_618) {
+				takeProfitCode = FibCode.FIB382;
+			} else if(openCode == FibCode.FIB2) {
 				takeProfitCode = FibCode.FIB5;
+			} else if(openCode == FibCode.FIB2_618) {
+				takeProfitCode = FibCode.FIB618;
+			} else if(openCode.gte(FibCode.FIB3_618)) {
+				takeProfitCode = FibCode.FIB786;
 			}
 			
 			double takeProfitCodeValue = childFibInfo.getFibValue(takeProfitCode);
