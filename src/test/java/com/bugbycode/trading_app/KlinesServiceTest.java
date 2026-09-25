e0e437fe29b97dbf3bd17293be22cd545f4d364b
bugbycode <bugbycode@gmail.com>
2026-09-25 12:41:21 +0800

    单元测试代码

--- src/test/java/com/bugbycode/trading_app/KlinesServiceTest.java
+++ src/test/java/com/bugbycode/trading_app/KlinesServiceTest.java
@@ -226,7 +226,7 @@ public class KlinesServiceTest {
     public void testFibInfo(){
         String pair = "ENAUSDT";
         //List<Klines> list_1d = klinesRepository.findLastKlinesByPair(pair, Inerval.INERVAL_1D,1500);
-        //List<Klines> list_trend = klinesRepository.findLastKlinesByPair(pair, Inerval.INERVAL_1H,1500);
+        List<Klines> list_trend = klinesRepository.findLastKlinesByPair(pair, Inerval.INERVAL_1H,1500);
         List<Klines> list = klinesRepository.findLastKlinesByPair(pair, Inerval.INERVAL_1H, 1500);
         List<Klines> list_15m = klinesRepository.findLastKlinesByPair(pair, Inerval.INERVAL_15M,1500);
 		
@@ -236,7 +236,7 @@ public class KlinesServiceTest {
         
         //logger.info(klines_list_1h);
         
-        FibInfoFactory factory = new FibInfoFactoryImpl(list, list_15m, PositionSide.SHORT);
+        FibInfoFactory factory = new FibInfoFactoryImpl(list_trend, list, list_15m);
         
         if(!(factory.isLong() || factory.isShort())) {
         	return;
